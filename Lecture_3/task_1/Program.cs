d1bc1f3ca7486f93b95ed9bd94eaab802968d2a9
Evgeniya <loiseau87bleu@gmail.com>
2023-02-16 00:43:48 +0400

    task_1 - conditional 4 methods

---
+++ Lecture_3/task_1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Вид 1 
+// ничего не принимают, ничего не возвращают
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();
+
+
+// Вид 2
+// что-то принимают, ничего не возвращают
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++; //инкремент, декримент
+    }
+}
+Method21(msg:"Текст", count: 4); //аргументы можно указывать в разном порядке
+
+
+//Вид 3
+// ничего не принимают, что то возвращают
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+
+//Вид 4
+// что-то принимают, что-то возвращают
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; //пустая строка
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "C#");
+Console.WriteLine(res);
