1ef0ce8a8332b853c294fb70f6de5bf7bb82cb3f
Evgeniya <loiseau87bleu@gmail.com>
2023-02-15 23:19:58 +0400

    task_4 - array fill method

---
+++ Lecture_2/task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// метод заполнения массива
+void FillArray(int[] collection) 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; //создай новый массив, в котором будет 10 элементов
+
+FillArray(array);
+PrintArray(array);
