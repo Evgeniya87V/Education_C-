fa508273fccc08142f0d3bc10f5052fc3b65daca
Evgeniya <loiseau87bleu@gmail.com>
2023-02-16 23:39:27 +0400

    task_5

---
+++ Lecture_3/task_5/Program.cs
@@ -0,0 +1,42 @@
+﻿// Упорядочить массив
+//Алгоритм минимального или максимального
+/* найти позицию минимального элемента в неотсортированной части массива
+произвести обмен этого значения со значением первой неотсортированной позиции 
+повторять пока есть неотсортированные элементы*/
+ int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+ void PrintArray(int[] array)
+ {
+    int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+    Console.WriteLine();
+ }
+
+ void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1 ; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+      {
+          if(array[i] < array[minPosition]) minPosition = j;
+          {
+            minPosition = j;
+          }
+}
+
+    int temporary = array[i];
+    array[i] = array[minPosition];
+    array[minPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+
