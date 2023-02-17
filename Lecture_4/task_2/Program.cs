3b6a1ffd3d026228c6a1a4c94b99d90c9890d4f8
Evgeniya <loiseau87bleu@gmail.com>
2023-02-17 21:59:59 +0400

    task_2 - matrix of numbers

---
+++ Lecture_4/task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Матрица из чисел
+
+void PrintArray(int[,]  matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)  //0-первое чило из [3,4]
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) //1 -второе число из[3,4]
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+//добавим метод который будет заполнять матрицу случайными числами
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); //[1;10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
