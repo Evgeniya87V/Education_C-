3aa4a85cb177d1cc3fd44a5c69e594e031bf4c76
Evgeniya <loiseau87bleu@gmail.com>
2023-02-18 19:04:59 +0400

    task_3 - black and white image

--- Lecture_4/task_3/Program.cs
+++ Lecture_4/task_3/Program.cs
@@ -31,34 +31,33 @@ int[,] pic = new int[,]
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 };
 
-void PrintArray(int[,]  matr)
+void PrintImage(int[,] image)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)  //0-первое чило из [3,4]
-    {
-        for (int j = 0; j < matr.GetLength(1); j++) //1 -второе число из[3,4]
-        {
-            Console.Write($"{matr[i, j]} ");
-        }
-    Console.WriteLine();
-    }
+ for (int i = 0; i < image.GetLength(0); i++)
+ {
+ for (int j = 0; j < image.GetLength(1); j++)
+ {
+ if(image[i,j] == 0) Console.Write($" ");
+ else Console.Write($"+");
+ }
+ Console.WriteLine();
+ }
 }
-//добавим метод который будет заполнять матрицу случайными числами
-void FillArray(int[,] matr)
+void FillImage(int row, int col)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            matr[i,j] = new Random().Next(1,10); //[1;10)
-        }
-    }
+ if (pic[row, col] == 0)
+ {
+ pic[row, col] = 1;
+ FillImage(row - 1, col);
+ FillImage(row, col - 1);
+ FillImage(row + 1, col);
+ FillImage(row, col + 1);
+ }
 }
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
 
-int[,] matrix = new int[3, 4];
-PrintArray(matrix);
-FillArray(matrix);
-Console.WriteLine();
-PrintArray(matrix);
 
 
 
