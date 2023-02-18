8427362d966784bad344619fb64a3baffebcd475
Evgeniya <loiseau87bleu@gmail.com>
2023-02-18 19:36:44 +0400

    task_4 - factorial

---
+++ Lecture_4/task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Факториал
+// 5! = 5 * 4 * 3 * 2 * 1
+//      5 * 4!
+//          4 * 3!
+
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine(Factorial(i));
+}
+
+//Console.WriteLine(Factorial(3)); // 1*2*3 = 6
