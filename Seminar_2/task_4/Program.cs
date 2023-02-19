c2cb98edd948df5d2e85c5ec2edd751d41234772
Evgeniya <loiseau87bleu@gmail.com>
2023-02-19 14:16:55 +0400

    task_4

---
+++ Seminar_2/task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая на вход принимает число и
+ проверяет кратно оно одновременно 7 и 23
+ */
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int enteredNumber = numberRequest("Enter number: ");
+
+if (enteredNumber % 7 == 0 && enteredNumber % 23 == 0)
+{
+    Console.WriteLine("Да");
+}
+else 
+Console.WriteLine($"Нет");
