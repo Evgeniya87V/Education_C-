1a44b8f17a2dc6af01cce9ef004a54438775c783
Evgeniya <loiseau87bleu@gmail.com>
2023-02-19 12:56:30 +0400

    task_3

---
+++ Seminar_2/task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выводить 
+является ли второе число кратное первому, если не кратное, то программа
+ выводит остаток от деления
+*/
+
+int numberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int numberOne = numberRequest("Введите первое число ");
+int numberTwo = numberRequest("Введите второе число ");
+
+if (numberOne % numberTwo == 0)
+{
+    Console.WriteLine("Кратно!");
+}
+else 
+Console.WriteLine($"Не кратно. Остаток от деления {numberOne % numberTwo}");
