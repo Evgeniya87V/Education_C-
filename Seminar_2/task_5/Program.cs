1e57ea9b4e8c5e84f901969f560ba7767f655114
Evgeniya <loiseau87bleu@gmail.com>
2023-02-19 16:36:34 +0400

    task_5

---
+++ Seminar_2/task_5/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу которая на вхлд принимает два числа и 
+проверяет является ли одно число квадратом другого
+*/
+int NumberRequest(string message)
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int numberOne = NumberRequest("Enter number: ");
+int numberTwo = NumberRequest("Enter number: ");
+
+if (numberOne * numberOne == numberTwo || numberTwo * numberTwo == numberOne)
+{
+    Console.WriteLine("Да");
+}
+else 
+Console.WriteLine($"Нет");
