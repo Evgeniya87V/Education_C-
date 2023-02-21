792d6ed245d078f606c5948e383b550ce5222b64
Evgeniya <loiseau87bleu@gmail.com>
2023-02-21 22:40:33 +0400

    task_4 - square number

---
+++ Seminar_3/task_4/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+*/
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result != 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("ВВели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int n = GetNumber("ВВедите число:");
+
+for (int i = 1; i <= Math.Abs(n); i++)
+{
+    Console.Write($"{Math.Pow(i,2)} ");
+}
