aa44d2b432f6f247d623fc607a1e4622991f650a
Evgeniya <loiseau87bleu@gmail.com>
2023-02-26 15:41:44 +0400

    task_2 - t-26 - the number of digits in a number

---
+++ Seminar_4/task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 26. Напишите программу, которая на вход принимает число  
+и выдает количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int GetCountOfDigits(int number)
+{
+    int count = 0;
+    while (number != 0)
+    {
+        number = number/10;
+        count++;
+    }
+    return count;
+}
+
+int number = GetNumber("Введите число больше 0: ");
+int countOfDigits = GetCountOfDigits(number);
+Console.WriteLine($"Введеное число: {number}, количество цифр в нем: {countOfDigits}");
