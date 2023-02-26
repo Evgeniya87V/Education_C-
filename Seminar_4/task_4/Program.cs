212712c95d2cbdf709e90a4bf82f4102569a9972
Evgeniya <loiseau87bleu@gmail.com>
2023-02-26 18:18:34 +0400

    task_4 - array of elements

---
+++ Seminar_4/task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая выводит массив из 8 элементов
+0 и 1 в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+int[] array = new int[8];
+
+void InitArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    Random rnd = new Random();
+    while (index < length)
+    {
+        array[index] = rnd.Next(0,2);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+InitArray(array);
+PrintArray(array);
